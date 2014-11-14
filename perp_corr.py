efc349074098eb1b0acf73f3ff9bc0b12c5eb4dd
ferdinandvwyk <ferdinandvwyk@gmail.com>
2014-11-14 13:27:24 +0000

    ---Ported perp_corr and time_corr to python3

--- perp_corr.py
+++ perp_corr.py
@@ -7,7 +7,7 @@ rhoref = 6.0791e-03
 
 # Read order is [lx, ly, kx, ky]
 perp = abs(np.genfromtxt('analysis/perp_fit.csv', delimiter=','))
-print perp.shape
+print(perp.shape)
 
 plt.plot(perp[:])
 plt.xlabel('Time Window')
@@ -15,18 +15,18 @@ plt.ylabel('Value (rho or rho^-1)')
 plt.yscale('log')
 plt.show()
 
-print 'lx = ', np.mean(np.mean(perp[:,0], axis=0))*rhoref
-print 'std(lx) = ', np.std(perp[:,0], axis=0)*rhoref
+print('lx = ', np.mean(np.mean(perp[:,0], axis=0))*rhoref, 'm')
+print('std(lx) = ', np.std(perp[:,0], axis=0)*rhoref, 'm')
 
-print 'ly = ', np.mean(np.mean(perp[:,1], axis=0))*rhoref
-print 'std(ly) = ', np.std(perp[:,1])*rhoref
+print('ly = ', np.mean(np.mean(perp[:,1], axis=0))*rhoref, 'm')
+print('std(ly) = ', np.std(perp[:,1])*rhoref, 'm')
 
-print 'kx = ', np.mean(np.mean(perp[:,2], axis=0))/rhoref
-print 'std(kx) = ', np.std(perp[:,2])/rhoref
+print('kx = ', np.mean(np.mean(perp[:,2], axis=0))/rhoref, 'm^-1')
+print('std(kx) = ', np.std(perp[:,2])/rhoref, 'm^-1')
 
-print 'ky = ', np.mean(np.mean(perp[:,3], axis=0))/rhoref
-print 'std(ky) = ', np.std(perp[:,3])/rhoref
+print('ky = ', np.mean(np.mean(perp[:,3], axis=0))/rhoref, 'm^-1')
+print('std(ky) = ', np.std(perp[:,3])/rhoref, 'm^-1')
 
-print 'theta = ', np.arctan(np.mean(np.mean(perp[:,2]/perp[:,3], axis=0)))
-print 'std(theta) = ', np.std(np.arctan(perp[:,2]/perp[:,3]))
+print('theta = ', np.arctan(np.mean(np.mean(perp[:,2]/perp[:,3], axis=0))))
+print('std(theta) = ', np.std(np.arctan(perp[:,2]/perp[:,3])))
 
--- time_corr.py
+++ time_corr.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pickle as pkl
 
 tau = np.genfromtxt('analysis/time_fit.csv', delimiter=',')
-print tau.shape
+print(tau.shape)
 tau = np.array(tau)
 
 plt.plot(np.transpose(tau[:]))
@@ -11,5 +11,5 @@ plt.xlabel('Minor Radius')
 plt.ylabel("Time Correlation $(\mu s)$")
 plt.show()
 
-print 'Average = ', np.mean(np.mean(tau, axis=0))
-print 'Standard Deviation = ', np.std(np.mean(tau, axis=0))
+print('Average = ', np.mean(np.mean(tau, axis=0)))
+print('Standard Deviation = ', np.std(np.mean(tau, axis=0)))
