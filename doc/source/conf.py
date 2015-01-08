91e1aee74f33820aa03cf936735566a301c4dc55
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-01-08 18:22:22 +0000

    small typo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,7 +46,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.pngmath',
     'sphinx.ext.viewcode',
-    'numpydoc'
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
