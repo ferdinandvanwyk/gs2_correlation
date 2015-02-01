b5a5ec4f54b42f0a22a43b8ff3b91a37e6288791
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-02-01 14:32:54 +0000

    remove wk.py

--- gs2_correlation/wk.py
+++
@@ -1,5 +0,0 @@
-def wk_thm_1d(field_1, field_2):
-    field = np.conjugate(field_1)*field_2
-    corr = np.fft.ifft(field)
-    # multiply by number of pts to normalize correctly. See README.
-    return corr.real*field.shape[0]
