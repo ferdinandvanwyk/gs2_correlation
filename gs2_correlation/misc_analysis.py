fix fft normalizations by removing factor of 2

gs2_correlation/misc_analysis.py
@@ -29,7 +29,7 @@ def field_to_real_space(field):
         field_real_space[it,:,:] = np.roll(field_real_space[it,:,:],
                                                 int(nx/2), axis=0)
 
-    return field_real_space*nx*ny*rho_star/2
+    return field_real_space*nx*ny*rho_star
 
 def plot_heat_flux(it):
     """

gs2_correlation/simulation.py
@@ -796,7 +796,7 @@ class Simulation(object):
             self.field_real_space[it,:,:] = np.roll(self.field_real_space[it,:,:],
                                                     int(self.nx/2), axis=0)
 
-        self.field_real_space = self.field_real_space*self.nx*self.ny/2
+        self.field_real_space = self.field_real_space*self.nx*self.ny
         self.field_real_space = self.field_real_space*self.rho_star
 
     def calculate_time_corr(self, it):

test/test_simulation.py
@@ -63,7 +63,7 @@ class TestClass(object):
     def test_zf_bes(self, run):
         assert (run.field[:, :, 0] == 0).all()
 
-    def test_to_complex(self, run):
+    def test_field_to_complex(self, run):
         assert np.iscomplexobj(run.field) == True 
 
     def test_wk_2d(self, run):